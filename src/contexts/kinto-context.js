03156005938ab361371331d2116c820c66b121f8
ubergesundheit <ubergesundheit@googlemail.com>
2019-06-02 19:30:27 +0200

    load kinto url from environment

--- src/contexts/kinto-context.js
+++ src/contexts/kinto-context.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import KintoClient from 'kinto-http';
 
-const SERVER_URL = 'http://localhost:8888/v1/';
+const SERVER_URL = process.env.REACT_APP_KINTO_URL; //eslint-disable-line no-undef
 
 const KintoContext = React.createContext();
 
