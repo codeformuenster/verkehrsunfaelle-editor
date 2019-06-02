c65475702fe15099196cd7a73548d635cc2df511
ubergesundheit <ubergesundheit@googlemail.com>
2019-06-02 13:40:58 +0200

    Add some top-margin to footer box

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import MuiLink from '@material-ui/core/Link';
 
 const Footer = () => (
-  <Grid container direction="row" justify="center" alignItems="center">
-    Ein Projekt von{' '}
-    <MuiLink href="https://codeformuenster.org/">
-      <img
-        alt="Code for Münster"
-        src="https://codeformuenster.org/img/cfm_logo.png"
-      />
-    </MuiLink>
-  </Grid>
+  <Box mt={3}>
+    <Grid container direction="row" justify="center" alignItems="center">
+      Ein Projekt von{' '}
+      <MuiLink href="https://codeformuenster.org/">
+        <img
+          alt="Code for Münster"
+          src="https://codeformuenster.org/img/cfm_logo.png"
+        />
+      </MuiLink>
+    </Grid>
+  </Box>
 );
 
 export default Footer;
