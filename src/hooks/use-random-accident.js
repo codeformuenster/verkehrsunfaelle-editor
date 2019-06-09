57c4883a162affdec688b53e057124165abafd6e
ubergesundheit <ubergesundheit@googlemail.com>
2019-06-09 21:03:42 +0200

    configure random-accident url using .env files

--- src/hooks/use-random-accident.js
+++ src/hooks/use-random-accident.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ky from 'ky';
 
+const SERVER_URL = process.env.REACT_APP_RANDOM_ACCIDENT_URL; //eslint-disable-line no-undef, max-len
+
 const useRandomAccident = () => {
   const [isLoading, setIsLoading] = React.useState(true);
   const [accident, setAccident] = React.useState({ error: false });
@@ -10,7 +12,7 @@ const useRandomAccident = () => {
   React.useEffect(() => {
     setIsLoading(true);
     const abortController = new AbortController();
-    ky.get('http://localhost:9000/hooks/random-accident', {
+    ky.get(`${SERVER_URL}/random-accident`, {
       signal: abortController.signal,
     })
       .json()
