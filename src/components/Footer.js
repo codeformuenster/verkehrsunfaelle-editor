dcaf3d5043cce2cd89bbe0864babe6353751a93e
ubergesundheit <ubergesundheit@googlemail.com>
2019-06-17 19:03:53 +0200

    make footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import MuiLink from '@material-ui/core/Link';
 
 const Footer = () => (
-  <Box mt={3}>
-    <Grid container direction="row" justify="center" alignItems="center">
-      Ein Projekt von{' '}
-      <MuiLink href="https://codeformuenster.org/">
-        <img
-          alt="Code for Münster"
-          src="https://codeformuenster.org/img/cfm_logo.png"
-        />
-      </MuiLink>
-    </Grid>
+  <Box
+    mt={3}
+    justifyContent="center"
+    alignItems="center"
+    display="flex"
+    flexDirection={{ xs: 'column', sm: 'row' }}
+  >
+    Ein Projekt von{' '}
+    <MuiLink href="https://codeformuenster.org/">
+      <img
+        alt="Code for Münster"
+        src="https://codeformuenster.org/img/cfm_logo.png"
+      />
+    </MuiLink>
   </Box>
 );
 
