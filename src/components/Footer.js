8fcf5c8d0d5d487a2b84b9228a60c0be7a8ae4e6
ubergesundheit <ubergesundheit@googlemail.com>
2019-10-03 16:08:23 +0200

    add Apache 2.0 LICENSE & version. Link to Impressum & Code in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,21 +3,38 @@ import Box from '@material-ui/core/Box';
 import MuiLink from '@material-ui/core/Link';
 
 const Footer = () => (
-  <Box
-    mt={3}
-    justifyContent="center"
-    alignItems="center"
-    display="flex"
-    flexDirection={{ xs: 'column', sm: 'row' }}
-  >
-    Ein Projekt von{' '}
-    <MuiLink href="https://codeformuenster.org/">
-      <img
-        alt="Code for Münster"
-        src="https://codeformuenster.org/img/cfm_logo.png"
-      />
-    </MuiLink>
-  </Box>
+  <>
+    <Box
+      mt={3}
+      justifyContent="center"
+      alignItems="center"
+      display="flex"
+      flexDirection={{ xs: 'column', sm: 'row' }}
+    >
+      Ein Projekt von{' '}
+      <MuiLink href="https://codeformuenster.org/">
+        <img
+          alt="Code for Münster"
+          src="https://codeformuenster.org/img/cfm_logo.png"
+        />
+      </MuiLink>
+    </Box>
+    <Box justifyContent="center" display="flex">
+      <Box display="flex" justifyContent="space-between" style={{ width: 300 }}>
+        <MuiLink
+          href="https://github.com/codeformuenster/verkehrsunfaelle-editor"
+          target="_blank"
+        >
+          Quellcode (Version{' '}
+          {/* eslint-disable no-undef */ process.env.REACT_APP_VERSION})
+        </MuiLink>
+        <span>|</span>
+        <MuiLink href="https://codeformuenster.org/impressum/" target="_blank">
+          Impressum
+        </MuiLink>
+      </Box>
+    </Box>
+  </>
 );
 
 export default Footer;
